cde2366af6e4f92820048ff3324e00a62313ca27
lynn2meng <lynn2meng@tamu.edu>
2020-08-13 16:10:03 -0500

    add GCS function

--- service/main.go
+++ service/main.go
@@ -1,6 +1,8 @@
 package main
 
 import(
+	"context"
+	"cloud.google.com/go/storage"
 	elastic "gopkg.in/olivere/elastic.v3"
 	"fmt"
 	"net/http"
@@ -9,6 +11,7 @@ import(
 	"strconv"
 	"reflect"
 	"github.com/pborman/uuid"
+	"io"
 )
 
 const (
@@ -18,7 +21,8 @@ const (
     // Needs to update
     //PROJECT_ID = "around-xxx"
     //BT_INSTANCE = "around-post"
-    ES_URL = "http://35.192.27.36:9200/"
+	ES_URL = "http://35.193.70.121:9200/"
+	BUCKET_NAME = "post-images-276919"
 
 )
 
@@ -32,6 +36,7 @@ type Post struct {
 	User string `json:"user"`
 	Message string `json:"message"`
 	Location Location `json:"location"`
+	Url    string `json:"url"`
 }
 
 
@@ -86,21 +91,90 @@ func main() {
 //  }
 //  
 func handlerPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Received one post request.")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 
-	decoder := json.NewDecoder(r.Body)
-	var p Post
-	if err := decoder.Decode(&p); err != nil {// err definiton, field scope is different
-		panic(err)
-	}
+	// 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
+	r.ParseMultipartForm(32 << 20)
+
+	//Parse form data
+	fmt.Printf("Received one post request %s\n", r.FormValue("message"))
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+    p := &Post{
+		User:    "1111",
+		Message: r.FormValue("message"),
+		Location: Location{
+			   Lat: lat,
+			   Lon: lon,
+		},
+ }
 
 	fmt.Fprintf(w, "Post received: %s\n", p.Message)// write the p.msg to w
 
 	id := uuid.New()
+
+	file, _, err := r.FormFile("image")
+    if err != nil {
+            http.Error(w, "Image is not available", http.StatusInternalServerError)
+            fmt.Printf("Image is not available %v.\n", err)
+            return
+    }
+    defer file.Close()
+
+	ctx := context.Background()
+	_, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
+    if err != nil {
+            http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+            fmt.Printf("GCS is not setup %v\n", err)
+            return
+    }
+
+	// Update the media link after saving to GCS.
+	p.Url = attrs.MediaLink
+
+
 	// Save to ES
-	saveToES(&p, id)
+	saveToES(p, id)
 
 }
+
+func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+	client, err := storage.NewClient(ctx)
+    if err != nil {
+            return nil, nil, err
+	}
+	
+	defer client.Close()
+
+	//create a bucket instannce
+	bucket := client.Bucket(bucketName)
+	// Next check if the bucket exists
+	if _, err = bucket.Attrs(ctx); err != nil {
+		return nil, nil, err
+	}
+
+	obj := bucket.Object(name)
+	w := obj.NewWriter(ctx)
+	
+	if _, err := io.Copy(w, r); err != nil {
+		return nil, nil, err
+ 	}
+ 	if err := w.Close(); err != nil {
+		return nil, nil, err
+ 	}
+
+	if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, nil, err
+ 	}
+
+ 	attrs, err := obj.Attrs(ctx)
+ 	fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
+ 	return obj, attrs, err
+
+}
+
 
 func saveToES(p *Post, id string) {
 	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL),elastic.SetSniff(false))
